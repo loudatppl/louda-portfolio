fde1fddcc0c00742ae89e72d8a2186e7ecc30f26
loudatppl <123815534+loudatppl@users.noreply.github.com>
2023-06-18 00:13:33 +0800

    feat: add new fonts

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav
+        className='flex fixed flex-col w-auto
+        h-screen right-0 top-0'
+    >
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+    </nav>
+  )
+}
+
+export default Nav
---
+++ src/components/Preloader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Preloader = () => {
+  return (
+    <div
+        className=''
+    >
+
+    </div>
+  )
+}
+
+export default Preloader
