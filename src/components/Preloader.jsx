d7052018ffdb12815e832f771ffc56abaf0e8267
loudatppl <123815534+loudatppl@users.noreply.github.com>
2023-06-21 21:42:22 +0800

    fix: preloader working

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -13,7 +13,7 @@ const Preloader = () => {
   return (
     <div
       className={`fixed w-full h-screen flex items-center justify-center bg-dwhite z-[999999] top-0 left-0 bottom-0 right-0 ${
-        fadeOut ? "fade-out" : ""
+        fadeOut ? "fade-out fix" : ""
       }`}
     >
       <div className="flex w-auto items-center">
