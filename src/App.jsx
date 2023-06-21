683fee1453af9c0906260b12a25392c9cfad5637
loudatppl <123815534+loudatppl@users.noreply.github.com>
2023-06-21 18:15:38 +0800

    fix delete portfolio page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React from "react";
 
-import Portfolio from './pages/Portfolio'
+import Nav from "./components/Nav";
+import Profile from "./components/Profile";
+import Tabs from "./components/Tabs";
+import Posts from "./components/Posts";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
+import { Route, Routes } from "react-router";
 
 const App = () => {
   return (
     <>
-      <Portfolio />
+      <header>
+        <Nav />
+        <Profile />
+      </header>
+      <main>
+        <Tabs />
+        <Routes>
+          <Route path="/" element={<Posts />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
