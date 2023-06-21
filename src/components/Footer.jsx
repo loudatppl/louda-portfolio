849a6287671d07c2c2eea8055b4f786fc5807231
loudatppl <123815534+loudatppl@users.noreply.github.com>
2023-06-21 22:19:20 +0800

    feat: added links to icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from "react";
 
-import { BsLinkedin, BsGithub } from 'react-icons/bs'
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <footer
-        className='w-full flex justify-center py-10 text-dblack'
-    >
+    <footer className="w-full flex justify-center py-10 text-dblack">
+      <div className="flex flex-col w-[768px] px-4 space-y-4">
         <div
-            className='flex flex-col w-[768px] px-4 space-y-4'
+          className="flex space-x-4 justify-center
+                cursor-pointer"
         >
-            <div
-                className='flex space-x-4 justify-center
-                cursor-pointer'
-            >
-                <BsGithub size={26} />
-                <BsLinkedin size={26} />
-            </div>
-            <div
-                className='flex justify-center'
-            >
-                <p
-                    className='md:text-sm text-xs cursor-pointer'
-                >LOUDAKRIS TUPPAL &copy; 2023</p>
-            </div>
+          <a href="https://github.com/loudatppl" target="_blank">
+            <BsGithub size={26} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/loudakris-tuppal-79b333258/"
+            target="_blank"
+          >
+            <BsLinkedin size={26} />
+          </a>
         </div>
+        <div className="flex justify-center">
+          <p className="md:text-sm text-xs cursor-pointer">
+            LOUDAKRIS TUPPAL &copy; 2023
+          </p>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
